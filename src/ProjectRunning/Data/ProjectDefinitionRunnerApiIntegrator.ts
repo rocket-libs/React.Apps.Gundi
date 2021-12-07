8995cffda4782d3f3cd14ecf95f8c76a2a82dab2
nyingi <nyingi.maina@gmail.com>
2021-12-07 17:58:07 +0300

    basics of event streaming

--- src/ProjectRunning/Data/ProjectDefinitionRunnerApiIntegrator.ts
+++ src/ProjectRunning/Data/ProjectDefinitionRunnerApiIntegrator.ts
@@ -3,12 +3,23 @@ import IProcessRunningResult from "../../Project/Data/IProcessRunningResult";
 export default class ProjectDefinitionRunnerApiIntegrator extends StandardApiIntergrator<object> {
   basePath: string = "Run";
 
+  eventSource: EventSource | undefined;
+
   public async runByIdAsync(
     projectId: string
   ): Promise<IProcessRunningResult[]> {
+    
+    this.eventSource = new EventSource(`http://localhost:5002/api/v1/EventStreaming/listen`);
+    this.eventSource.onmessage = (event) => {
+      console.log(event.data);
+    }
+
+
     const result = await this.getAsync<IProcessRunningResult[]>(
       `run-by-id?projectId=${projectId}`
     );
     return result;
   }
+
+  
 }
